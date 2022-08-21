ee578580086c09ff213296c073c3e439f34b5f6e
AM Pillai <pillai_amal@htomail.com>
2022-08-21 11:19:50 +0530

    command line args

---
+++ temp/echor/target/debug/deps/echor-1b4a016969390b90.d
@@ -0,0 +1,5 @@
+/home/pillai_amal/github.com/learningRust/temp/echor/target/debug/deps/echor-1b4a016969390b90: src/main.rs
+
+/home/pillai_amal/github.com/learningRust/temp/echor/target/debug/deps/echor-1b4a016969390b90.d: src/main.rs
+
+src/main.rs:
---
+++ temp/echor/target/debug/echor.d
@@ -0,0 +1,1 @@
+/home/pillai_amal/github.com/learningRust/temp/echor/target/debug/echor: /home/pillai_amal/github.com/learningRust/temp/echor/src/main.rs
